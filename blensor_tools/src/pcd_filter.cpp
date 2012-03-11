8ebac437b0a9486b57ec3c697a025e41204225c0
xmgschwan <xmgschwan@caladrius.(none)>
2012-03-11 10:36:23 +0100

    PCD combiner does no more filtering The filtering is now done by pcd_filter

--- blensor_tools/src/pcd_filter.cpp
+++ blensor_tools/src/pcd_filter.cpp
@@ -4,6 +4,7 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/voxel_grid.h>
 #include <string>
+#include <cstdio>
 
 using namespace std;
 
@@ -14,7 +15,6 @@ int
   bool binary = false;
 
   pcl::PointCloud<pcl::PointXYZ> *cloud1(new  pcl::PointCloud<pcl::PointXYZ>());
-  pcl::PointCloud<pcl::PointXYZ> cloud2;
   pcl::PointCloud<pcl::PointXYZ> outputCloud;
   pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud1_ptr(cloud1);
 
@@ -22,19 +22,20 @@ int
 
   pcl::PCDReader reader;
   reader.read(string(argv[1]),*cloud1);
-  reader.read(string(argv[2]),cloud2);
-  if (argc > 3)
-  {
-    if (string(argv[3]) == "binary") binary = true;
-  } 
+  float leafsize = 0.01;
+  sscanf(argv[2],"%f",&leafsize);
+  if (string(argv[4]) == "binary") binary = true;
 
+  pcl::VoxelGrid<pcl::PointXYZ> grid;
+  grid.setLeafSize(leafsize,leafsize,leafsize);
+  grid.setInputCloud(cloud1_ptr);
+  grid.filter(outputCloud);
 
-  *cloud1 += cloud2;
-
+  std::cout << "Cloud size befor/after:  "<<cloud1->size()<<"/"<<outputCloud.size() << std::endl;
   
   pcl::PCDWriter writer;
   cout << "Writing to " << argv[3] << endl;
-  writer.write(string(argv[3]), *cloud1, binary);
+  writer.write(string(argv[3]), outputCloud, binary);
   cout << "Finished"<<endl;
   return (0);
 }
