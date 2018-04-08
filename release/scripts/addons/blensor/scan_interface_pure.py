9842ef58875705fb032732de919e9fe6174b8adf
developer <mgschwan@zero997.com>
2018-04-08 13:19:06 +0200

    Add basic shading in fallback scanner

--- release/scripts/addons/blensor/scan_interface_pure.py
+++ release/scripts/addons/blensor/scan_interface_pure.py
@@ -1,5 +1,3 @@
-from .octrees import octrees
-from .octrees import blob_octrees
 import bpy
 from mathutils import Matrix, Vector
 from mathutils.bvhtree import BVHTree
@@ -27,7 +25,7 @@ def scan(numberOfRays, max_distance, elementsPerRay, keep_render_setup, do_shadi
     # Step 1: Scene to polygons / store face indices and materials
     tris = []
     faces = []
-    face_array,obj_array = scene_to_mesh()
+    face_array,obj_array,mat_array = scene_to_mesh()
 
     for idx,f in enumerate(face_array):
         tris.append(list(f[0]))
@@ -43,6 +41,8 @@ def scan(numberOfRays, max_distance, elementsPerRay, keep_render_setup, do_shadi
 
     origin = Vector([0.0,0.0,0.0])
     direction = Vector([0.0,0.0,0.0])
+
+    hit_indices = [-1]*numberOfRays
 
     for idx in range(numberOfRays):
         direction.x = rays_buffer[idx*elementsPerRay]
@@ -72,9 +72,11 @@ def scan(numberOfRays, max_distance, elementsPerRay, keep_render_setup, do_shadi
             name = obj.name
             returns_buffer[idx*ELEMENTS_PER_RETURN+4] = ord(name[0]) + (ord(name[1])<<8) + (ord(name[2])<<16) + (ord(name[3])<<24)
             
-            returns_buffer[idx*ELEMENTS_PER_RETURN+5] = 255.0
-            returns_buffer[idx*ELEMENTS_PER_RETURN+6] = 255.0
-            returns_buffer[idx*ELEMENTS_PER_RETURN+7] = 255.0
+            returns_buffer[idx*ELEMENTS_PER_RETURN+5] = 1.0
+            returns_buffer[idx*ELEMENTS_PER_RETURN+6] = 1.0
+            returns_buffer[idx*ELEMENTS_PER_RETURN+7] = 1.0
+
+            hit_indices[idx] = hit_idx
 
         else:
             for r in range(ELEMENTS_PER_RETURN):
@@ -83,7 +85,15 @@ def scan(numberOfRays, max_distance, elementsPerRay, keep_render_setup, do_shadi
     # Step 3: Shade rays
     # TODO: Implement material solver
     if do_shading:
-        pass
+        for idx,mat_idx in enumerate(hit_indices):
+            if mat_idx >= 0:
+                color = mat_array[mat_idx].diffuse_color
+                returns_buffer[idx*ELEMENTS_PER_RETURN+5] = color.r
+                returns_buffer[idx*ELEMENTS_PER_RETURN+6] = color.g
+                returns_buffer[idx*ELEMENTS_PER_RETURN+7] = color.b
+
+
+            
 
 def scene_to_mesh():
     # TODO: Return materials per face as well
@@ -91,16 +101,18 @@ def scene_to_mesh():
 
     scene_faces = []
     scene_obj = []
+    scene_materials = []
 
     idx = 0
     for ob in bpy.data.objects:
-        faces = faces_from_mesh(ob,Matrix(), use_mesh_modifiers=True, triangulate=True)
+        faces = faces_from_mesh(ob,Matrix(), use_mesh_modifiers=True)
         for f in faces:
-            scene_faces.append(list(map(list,f)))
+            scene_faces.append(list(map(list,f[0:3])))
             scene_obj.append(ob)
-    return scene_faces, scene_obj
+            scene_materials.append(f[3])
+    return scene_faces, scene_obj, scene_materials
 
-def faces_from_mesh(ob, global_matrix, use_mesh_modifiers=False, triangulate=True):
+def faces_from_mesh(ob, global_matrix, use_mesh_modifiers=False):
     """
     From an object, return a generator over a list of faces.
 
@@ -129,57 +141,22 @@ def faces_from_mesh(ob, global_matrix, use_mesh_modifiers=False, triangulate=Tru
         mesh.flip_normals()
         mesh.calc_tessface()
 
-    if triangulate:
-        # From a list of faces, return the face triangulated if needed.
-        def iter_face_index():
-            for face in mesh.tessfaces:
-                vertices = face.vertices[:]
-                if len(vertices) == 4:
-                    yield vertices[0], vertices[1], vertices[2]
-                    yield vertices[2], vertices[3], vertices[0]
-                else:
-                    yield vertices
-    else:
-        def iter_face_index():
-            for face in mesh.tessfaces:
-                yield face.vertices[:]
+    def iter_face_index():
+        for face in mesh.tessfaces:
+            vertices = face.vertices[:]
+            material = None
+            if face.material_index < len(ob.material_slots):
+                material = ob.material_slots[face.material_index].material
+            if len(vertices) == 4:
+                yield vertices[0], vertices[1], vertices[2],material
+                yield vertices[2], vertices[3], vertices[0],material
+            else:
+                yield vertices[0],vertices[1],vertices[2],material
 
     vertices = mesh.vertices
 
-    for indexes in iter_face_index():
-        yield [vertices[index].co.copy() for index in indexes]
+    for face in iter_face_index():
+        yield [vertices[face[0]].co.copy(), vertices[face[1]].co.copy(), vertices[face[2]].co.copy(), face[3]]
 
     bpy.data.meshes.remove(mesh)
 
-
-
-def insert_face_into_tree(face, tree):
-    """Calculate the center and bounds and insert the triangle into the tree 
-       with the given bounds and the center as the index"""
-    x = y = z = 0
-    minx = maxx = face[0][0]
-    miny = maxy = face[0][1]
-    minz = maxz = face[0][2]
-
-    for vertex in face:
-        x += vertex[0]
-        y += vertex[0]
-        z += vertex[0]
-        minx = min(minx, vertex[0])
-        miny = min(miny, vertex[1])
-        minz = min(minz, vertex[2])
-        maxx = max(maxx, vertex[0])
-        maxy = max(maxy, vertex[1])
-        maxz = max(maxz, vertex[2])
-    x = x/len(face) + len(tree)*machineEpsilon #try to make the triangles unique
-    y = y/len(face)    
-    z = z/len(face)    
-    tree.insert((x,y,z),((minx,maxx),(miny,maxy),(minz,maxz)),face)
-
-def test():
-    scene = [[[1.0, 0.9999999403953552, -1.0], [1.0, -1.0, -1.0], [-1.0000001192092896, -0.9999998211860657, -1.0]], [[-1.0000001192092896, -0.9999998211860657, -1.0], [-0.9999996423721313, 1.0000003576278687, -1.0], [1.0, 0.9999999403953552, -1.0]], [[1.0000004768371582, 0.999999463558197, 1.0], [-0.9999999403953552, 1.0, 1.0], [-1.0000003576278687, -0.9999996423721313, 1.0]], [[-1.0000003576278687, -0.9999996423721313, 1.0], [0.9999993443489075, -1.0000005960464478, 1.0], [1.0000004768371582, 0.999999463558197, 1.0]], [[1.0, 0.9999999403953552, -1.0], [1.0000004768371582, 0.999999463558197, 1.0], [0.9999993443489075, -1.0000005960464478, 1.0]], [[0.9999993443489075, -1.0000005960464478, 1.0], [1.0, -1.0, -1.0], [1.0, 0.9999999403953552, -1.0]],
-             [[1.0, -1.0, -1.0], [0.9999993443489075, -1.0000005960464478, 1.0], [-1.0000003576278687, -0.9999996423721313, 1.0]], [[-1.0000003576278687, -0.9999996423721313, 1.0], [-1.0000001192092896, -0.9999998211860657, -1.0], [1.0, -1.0, -1.0]], [[-1.0000001192092896, -0.9999998211860657, -1.0], [-1.0000003576278687, -0.9999996423721313, 1.0], [-0.9999999403953552, 1.0, 1.0]], [[-0.9999999403953552, 1.0, 1.0], [-0.9999996423721313, 1.0000003576278687, -1.0], [-1.0000001192092896, -0.9999998211860657, -1.0]], [[1.0000004768371582, 0.999999463558197, 1.0], [1.0, 0.9999999403953552, -1.0], [-0.9999996423721313, 1.0000003576278687, -1.0]], [[-0.9999996423721313, 1.0000003576278687, -1.0], [-0.9999999403953552, 1.0, 1.0], [1.0000004768371582, 0.999999463558197, 1.0]]]
-
-    tree = blob_octrees.BlobOctree(((-100.0,100.0),(-100.0,100.0),(-100.0,100.0)))
-    for face in scene:
-        insert_face_into_tree(face,tree)
